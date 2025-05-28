0aa8ed925b01f8a227e7f179f00141296b57874b
SaffiSardar <saffisardar2012@gmail.com>
2025-05-28 16:39:40 +0500

    Query Parameters - intro

--- main.py
+++ main.py
@@ -84,6 +84,28 @@
 
 #----->Query PARAMETERS
 
+# intro - mainly for searching/filtering/etc
+from fastapi import FastAPI
+
+app = FastAPI()
+
+fake_item_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+@app.get("/items/")
+async def read_item(skip:int=0,limit:int=10):
+    return fake_item_db[skip:skip+limit]
+
+
+# optional parameters - using none
+
+
+
+
+
+
+
+
+
 
 
 
